106912c34c3b083c7326242d384771ff369e26cb
0p4tr1k0 <66717846+0p4tr1k0@users.noreply.github.com>
2023-11-22 10:28:11 +0100

    Delete FPR/hw1 directory

--- FPR/hw1/program.hs
+++
@@ -1,4 +0,0 @@
-decode :: String -> [(Char, String)] -> String
-decode [] _ = []  -- Konečný případ: prázdný vstupní řetězec
-decode (x:xs) dictionary = case lookup x dictionary of Just originalPair -> originalPair ++ decode xs dictionary
-    Nothing -> x : decode xs dictionary
