ec9a9d5b2c0160fe183220cf691baf7eeef64d9b
0p4tr1k0 <66717846+0p4tr1k0@users.noreply.github.com>
2023-11-29 10:28:35 +0100

    Add files via upload

---
+++ fpr/cv2/program.hs
@@ -0,0 +1,34 @@
+data Point = Point{x :: Int, y :: Int}
+data Position = Position {topLeftCorner :: Point, width :: Int, height :: Int}
+
+data Component = TextBox {name :: String, position :: Position, text :: String}|
+                Button {name :: String, position :: Position, text :: String}|
+                Container {name :: String, children :: [Component]}
+
+gui :: Component
+gui = Container "Moje GUI" [
+    Container "Menu" [
+        Button "Button_new" (Position (Point 10 10)10 20) "New",
+        Button "Button_save" (Position (Point 10 50)10 20) "Save"
+    ],
+    Container "Page" [
+        TextBox "tb_1" (Position (Point 10 150)100 20) "Muj text"
+    ],
+    Container "Footer" []
+    ]
+
+instance Show Component where
+    show component = plshelp component 0 where
+        plshelp (Button name position text) tabs = replicate tabs '\t' ++ text ++ "\n"
+        plshelp (TextBox name postion text) tabs = replicate tabs '\t' ++ text ++ "\n"
+        plshelp (Container name children) tabs = replicate tabs '\t' ++ name ++ "\n" ++ concatMap (\x -> plshelp x (tabs+1)) children
+
+identify :: Component -> Component
+identify (Container n children) = Container n [identify c| c <- children]
+identify (TextBox n p t) = TextBox n p t
+identify (Button n p t) = Button n p t
+
+insertInto :: Component -> String -> Component -> Component
+insertInto (Container n children) name comp = Container n [insertInto c|c <- children]
+insertInto (TextBox n p t) name comp = (TextBox n p t)
+insertInto (Button n p t) name comp = (Button n p t)
